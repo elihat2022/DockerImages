7da387adaeb4349bb9edc9b5213415340d258107
Elihat Caceres <cacereselihat@gmail.com>
2025-03-30 12:48:00 +0000

    crud

--- Lesson1/users.py
+++ Lesson1/users.py
@@ -15,17 +15,61 @@ user_example = [User(id=1,name="Elihat", surname="Eli", url="https://eli.com", a
                 User(id=2,name="Ali", surname="Ali", url="https://ali.com", age=30),
                 User(id=3,name="Sara", surname="Ali", url="https://sara.com", age=22),]
 
+# Path
 @app.get("/users")
 async def users():
     return user_example
     
-@app.get("/user/{user_id}")
-async def user(user_id: int):
-    filtered_user = filter(lambda x: x.id == user_id, user_example)
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+
+@app.get("/user/")
+async def userQuery(id: int):
+    return search_user(id)
+
+@app.post("/user/")
+async def create_user(user: User):
+    if type(search_user(user.id))==User:
+        print(search_user(user.id))
+        return {"error":"User already exists"}
+ 
+    else:
+        user_example.append(user)
+        return {"New user created"}
+
+@app.put("/user/")
+async def user_update(user: User):
+    for index, user_saved in  enumerate(user_example):
+        try: 
+            if user_saved.id == user.id:
+                user_example[index] = user
+                return {"User updated"}
+        except:
+            return {"error": "User not found"}
+
+
+@app.delete("/user/")
+def delete_user(id: int):
+    user = search_user(id)
+    for index, user in enumerate(user_example):
+        try:
+            if user.id == id:
+             user_example.pop(index)
+             return {f"The user {user.name} was deleted"}
+        except:
+            return {"error": "user not found"}
+
+def search_user(id: int):
+    filtered_user = filter(lambda x: x.id == id, user_example)
     try:
         return list(filtered_user)[0]
     except IndexError:
         return {"error": "User not found"}
+    except ValueError:
+        return {"error": "Invalid ID format"}
+
+
 
 if __name__ == "__main__":
     import uvicorn
